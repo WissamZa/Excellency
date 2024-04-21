6923992cf23ca1294d6eb3caea2d9115216fb607
WissamZa <36477299+WissamZa@users.noreply.github.com>
2024-04-21 14:17:16 +0300

    Delete create_superuser.bat

--- create_superuser.bat
+++
@@ -1,4 +0,0 @@
-python manage.py makemigrations
-python manage.py migrate
-echo "from account.models import User; User.objects.create_superuser('admin', 'admin@Excellency.com', 'admin123')" | python manage.py shell
-python manage.py runserver
