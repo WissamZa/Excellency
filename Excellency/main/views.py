92c887414368400466b017cf4964e67b12bc9f0c
WissamZaidi <wzaidi@stu.kau.edu.sa>
2024-04-22 15:04:52 +0300

    Co-authored-by: Rayan alshehri <Rayan-glitchw@users.noreply.github.com> Co-authored-by: Mohammed Abdullah  <Mohammed9090909@users.noreply.github.com>

--- Excellency/main/views.py
+++ Excellency/main/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 
 def index_view(request: HttpRequest):
    return render(request, "main/index.html")
+
+
+def contactus_view(request: HttpRequest):
+   return render(request, "main/contactus.html")
+
+
+def lawyers_view(request: HttpRequest):
+   return render(request, "main/lawyers.html")
