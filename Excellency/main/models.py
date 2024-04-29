42e521cf75b23c3c66d5db3a35733d0590d550e5
Mohammed Alyahya <modezdf@gmail.com>
2024-04-29 12:23:32 +0300

    √

--- Excellency/main/models.py
+++ Excellency/main/models.py
@@ -9,12 +9,15 @@ class Contactus(models.Model):
    subject = models.CharField(max_length=120, blank=False)
    message = models.TextField(blank=False)
    created_at = models.DateTimeField(auto_now_add=True)
+   
+def post_image_upload_to(instance, filename):
+   return "post/images/{}/{}".format(instance.user.id, filename)
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField(upload_to='post_images')
+    image = models.ImageField(upload_to=post_image_upload_to)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
