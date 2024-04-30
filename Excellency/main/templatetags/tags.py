51daddf8a0da68d59731877a5ac546fcc0dbf62a
WissamZaidi <wzaidi@stu.kau.edu.sa>
2024-05-01 00:32:44 +0300

    modified:   Excellency/main/templates/main/lawyers.html 	modified:   Excellency/main/templatetags/tags.py 	modified:   Excellency/service/templates/service/chat.html 	modified:   Excellency/service/templates/service/order_details.html

--- Excellency/main/templatetags/tags.py
+++ Excellency/main/templatetags/tags.py
@@ -1,25 +1,39 @@
 from django import template
+from account.models import User
 register = template.Library()
 
 
 @register.filter(name="getlist")
 def getlist(request_dictionary, key):
-   return request_dictionary.getlist(key)
+    return request_dictionary.getlist(key)
+
+
+@register.filter(name="rate_average")
+def rate_average(user: User):
+    orders = user.lawyer.all()
+    order_count = orders.count()
+    if order_count == 0:
+        return 0
+    sum_rating = 0
+    for order in orders:
+        sum_rating += order.rating.rate
+    aver = sum_rating / order_count
+    return aver
 
 
 @register.filter(name='range')
 def _range(_min, max):
-   return range(1, max + 1)
-   # @register.filter(name='range')
-   # def _range(_min, args=None):
-   #    _max, _step = None, None
-   #    if args:
-   #       if not isinstance(args, int):
-   #          _max, _step = map(int, args.split(','))
-   #       else:
-   #          _max = args + 1
-   #    args = filter(None, (_min, _max, _step))
-   #    return range(*args)
+    return range(1, max + 1)
+    # @register.filter(name='range')
+    # def _range(_min, args=None):
+    #    _max, _step = None, None
+    #    if args:
+    #       if not isinstance(args, int):
+    #          _max, _step = map(int, args.split(','))
+    #       else:
+    #          _max = args + 1
+    #    args = filter(None, (_min, _max, _step))
+    #    return range(*args)
 
 
 # @register.filter(name='base_category')
@@ -29,12 +43,12 @@ def _range(_min, max):
 
 @register.filter(name='image_name')
 def image_name(name: str):
-   return name.split("/")[-1]
+    return name.split("/")[-1]
 
-   # @register.filter(name="random")
-   # def random_int(a):
-   #    return random.choice(plant_count())[0]
+    # @register.filter(name="random")
+    # def random_int(a):
+    #    return random.choice(plant_count())[0]
 
-   # @register.simple_tag
-   # def number_of_plants(request):
-   #    return plant_count() - 1
+    # @register.simple_tag
+    # def number_of_plants(request):
+    #    return plant_count() - 1
