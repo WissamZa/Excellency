1923353b3da763ac8ffa83835edc31eff0598b93
WissamZaidi <wzaidi@stu.kau.edu.sa>
2024-04-30 23:40:23 +0300

    Add rating to lawyers page admin view access perms

--- Excellency/main/views.py
+++ Excellency/main/views.py
@@ -4,132 +4,140 @@ from main.models import Contactus, Post, Like, Bookmark
 from service.models import Service
 from account.models import User, Specialty, LawyerProfile
 from django.db.models import Count
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 
 
 def index_view(request: HttpRequest):
-   order_num = Service.objects.all().count()
-   lawyers_num = User.objects.filter(role="Lawyer").count()
-   customar_num = User.objects.filter(role="Customar").count()
-   return render(request, "main/index.html", {"order_num": order_num,
-                                              "lawyers_num": lawyers_num,
-                                              "customar_num": customar_num})
+    order_num = Service.objects.all().count()
+    lawyers_num = User.objects.filter(role="Lawyer").count()
+    customar_num = User.objects.filter(role="Customar").count()
+    return render(request, "main/index.html", {"order_num": order_num,
+                                               "lawyers_num": lawyers_num,
+                                               "customar_num": customar_num})
 
 
 def contactus_view(request: HttpRequest):
-   msg = None
-   try:
-      if request.method == "POST":
-         Contactus.objects.create(full_name=request.POST.get('full_name'),
-                                  email=request.POST.get('email'),
-                                  subject=request.POST.get('subject'),
-                                  message=request.POST.get('message'),
-                                  )
-         msg = "تم ارسال رسالتك بنجاح"
-         return redirect("main:contact_success")
-   except Exception as e:
-      msg = e
-   return render(request, "main/contactus.html", {'msg': msg})
+    msg = None
+    try:
+        if request.method == "POST":
+            Contactus.objects.create(full_name=request.POST.get('full_name'),
+                                     email=request.POST.get('email'),
+                                     subject=request.POST.get('subject'),
+                                     message=request.POST.get('message'),
+                                     )
+            msg = "تم ارسال رسالتك بنجاح"
+            return redirect("main:contact_success")
+    except Exception as e:
+        msg = e
+    return render(request, "main/contactus.html", {'msg': msg})
 
 
-def check_lawyer_has_specialty(lawyer: User, specialty):
-   if specialty in lawyer.lawyer_profile.specialty.all():
-      return True
-   return False
+# def check_lawyer_has_specialty(lawyer: User, specialty):
+#     if specialty in lawyer.lawyer_profile.specialty.all():
+#         return True
+#     return False
 
 
 def lawyers_view(request: HttpRequest):
-   try:
-      lawyers = User.objects.filter(
-         role="Lawyer", lawyer_profile__certified=True).order_by("full_name")
-      spcialities = Specialty.objects.all()
+    try:
+        lawyers = User.objects.filter(
+            role="Lawyer", lawyer_profile__certified=True).order_by("id")
+        spcialities = Specialty.objects.all()
 
-      if "lawyer_name" in request.GET:
-         lawyers = lawyers.filter(
-            full_name__contains=request.GET.get("lawyer_name")
-         )
-      if "spcialties" in request.GET:
-         spcialities_filter = request.GET.getlist("spcialties")
-         lawyers = lawyers.filter(
-            lawyer_profile__specialty__in=spcialities_filter).annotate(Count("id"))
-      if "sort" in request.GET:
-         if request.GET.get("sort") == "user_name_a_z":
-            lawyers = lawyers.order_by("full_name")
-         if request.GET.get("sort") == "user_name_z-a":
-            lawyers = lawyers.order_by("-full_name")
-         if request.GET.get("sort") == "rating_top":
-            pass
-   except Exception as e:
-      print(e)
-   return render(request, "main/lawyers.html", {"lawyers": lawyers, "specialities": spcialities})
+        if "lawyer_name" in request.GET:
+            lawyers = lawyers.filter(
+                full_name__contains=request.GET.get("lawyer_name")
+            )
+        if "spcialties" in request.GET:
+            spcialities_filter = request.GET.getlist("spcialties")
+            lawyers = lawyers.filter(
+                lawyer_profile__specialty__in=spcialities_filter).annotate(Count("id"))
+        if "sort" in request.GET:
+            if request.GET.get("sort") == "user_name_a_z":
+                lawyers = lawyers.order_by("full_name")
+            if request.GET.get("sort") == "user_name_z-a":
+                lawyers = lawyers.order_by("-full_name")
+            if request.GET.get("sort") == "rating_top":
+                pass
+    except Exception as e:
+        print(e)
+    return render(request, "main/lawyers.html", {"lawyers": lawyers, "specialities": spcialities})
 
 
+@login_required(login_url='/account/login')
 def contact_messages(request: HttpRequest):
+    if not (request.user.is_staff or request.user.is_superuser):
+        return render(request, "no_permission.html")
+    messages = Contactus.objects.all()
 
-   messages = Contactus.objects.all()
-
-   return render(request, 'main/contact_messages.html', {'messages': messages})
+    return render(request, 'main/contact_messages.html', {'messages': messages})
 
 
-def admin_viwe(request):
-   lawyer_profiles = LawyerProfile.objects.all()
-   return render(request, 'main/admin.html', {'lawyer_profiles': lawyer_profiles})
+@login_required(login_url='/account/login')
+def admin_view(request):
+    if not (request.user.is_staff or request.user.is_superuser):
+        return render(request, "no_permission.html")
+    lawyer_profiles = LawyerProfile.objects.all()
+    return render(request, 'main/admin.html', {'lawyer_profiles': lawyer_profiles})
 
 
 def lawyer_details_view(request, user_id):
-   user = User.objects.get(id=user_id)
-   return render(request, 'main/lawyer_details.html', {"user": user})
+    user = User.objects.get(id=user_id)
+    return render(request, 'main/lawyer_details.html', {"user": user})
 
 
+@login_required(login_url='/account/login')
 def verification(request: HttpRequest, user_id):
-   user = User.objects.get(id=user_id)
-   user.lawyer_profile.certified = True
-   user.lawyer_profile.save()
-   return redirect("main:admin_viwe")
+    if not (request.user.is_staff or request.user.is_superuser):
+        return render(request, "no_permission.html")
+    user = User.objects.get(id=user_id)
+    user.lawyer_profile.certified = True
+    user.lawyer_profile.save()
+    return redirect("main:admin_view")
 
 
 def post_list(request):
-   if request.user.is_authenticated:
-      posts = Post.objects.all()
-      return render(request, 'main/post_lawyers.html', {'posts': posts})
-   else:
-      return redirect('account:login_view')
+    if request.user.is_authenticated:
+        posts = Post.objects.all()
+        return render(request, 'main/post_lawyers.html', {'posts': posts})
+    else:
+        return redirect('account:login_view')
 
 
 def like_post(request, post_id):
-   post = get_object_or_404(Post, pk=post_id)
-   user = request.user
-   if user.is_authenticated:
-      like, created = Like.objects.get_or_create(user=user, post=post)
-      if not created:
-         like.delete()
-   return redirect(reverse('main:post_list'))
-
+    post = get_object_or_404(Post, pk=post_id)
+    user = request.user
+    if user.is_authenticated:
+        like, created = Like.objects.get_or_create(user=user, post=post)
+        if not created:
+            like.delete()
+    return redirect(reverse('main:post_list'))
 
 
 def bookmark_post(request: HttpRequest, post_id):
 
     if not request.user.is_authenticated:
         return redirect("account:login_view")
-    
+
     try:
         post = Post.objects.get(pk=post_id)
 
-       
         bookmarked_post = Bookmark.objects.filter(user=request.user, post=post).first()
 
         if not bookmarked_post:
-            
+
             bookmark = Bookmark(user=request.user, post=post)
             bookmark.save()
         else:
-            
+
             bookmarked_post.delete()
-    
+
     except Exception as e:
         print(e)
 
     return redirect("main:post_list")
 
+
 def contact_success(request):
-     return render(request, 'main/contact_success.html')
+    return render(request, 'main/contact_success.html')
--- Excellency/service/models.py
+++ Excellency/service/models.py
@@ -13,63 +13,63 @@ Specialty_CHOICES = {
 
 
 def service_file_upload_to(instance, filename):
-   return "service/file/{}/{}".format(instance.id, filename)
+    return "service/file/{}/{}".format(instance.id, filename)
 
 
 class Service(models.Model):
-   completed = "مكتمل"
-   accepted = "مقبول"
-   pending_accept_offer = "انتظار قبول السعر"
-   pending = "قيد الانتظار"
-   rejected = "مرفوض"
-   STATUS_CHOICES = (
-      ('completed', completed),
-       ('accepted', accepted),
-       ('pending_accept_offer', pending_accept_offer),
-       ('pending', pending),
-       ('rejected', rejected),
+    completed = "مكتمل"
+    accepted = "مقبول"
+    pending_accept_offer = "انتظار قبول السعر"
+    pending = "قيد الانتظار"
+    rejected = "مرفوض"
+    STATUS_CHOICES = (
+        ('completed', completed),
+        ('accepted', accepted),
+        ('pending_accept_offer', pending_accept_offer),
+        ('pending', pending),
+        ('rejected', rejected),
     )
-   lawyer = models.ForeignKey(
-      User, on_delete=models.CASCADE, related_name="lawyer")
-   customar = models.ForeignKey(
-      User, on_delete=models.CASCADE, related_name="customar")
-   order_type = models.ForeignKey(Specialty, on_delete=models.CASCADE)
-   subject = models.CharField(max_length=100)
-   content = models.TextField()
-   file = models.FileField(
-      upload_to=service_file_upload_to, blank=True, null=True)
-   price = models.FloatField(
-       blank=True, null=True)
-   status = models.CharField(
-      max_length=18, default=pending)
-   created_date = models.DateTimeField(auto_now_add=True)
+    lawyer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="lawyer")
+    customar = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="customar")
+    order_type = models.ForeignKey(Specialty, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100)
+    content = models.TextField()
+    file = models.FileField(
+        upload_to=service_file_upload_to, blank=True, null=True)
+    price = models.FloatField(
+        blank=True, null=True)
+    status = models.CharField(
+        max_length=18, default=pending)
+    created_date = models.DateTimeField(auto_now_add=True)
 
 
 def group_based_upload_to(instance, filename):
-   return "profiles/file/{}/{}".format(instance.user.id, filename)
+    return "profiles/file/{}/{}".format(instance.user.id, filename)
 
 
 class Comment(models.Model):
-   service = models.ForeignKey(
-      Service, on_delete=models.CASCADE, related_name="chat")
-   user = models.ForeignKey(User, on_delete=models.CASCADE)
-   content = models.TextField()
-   file = models.FileField(
-      upload_to=group_based_upload_to, blank=True, null=True)
-   added_date = models.DateTimeField(auto_now_add=True)
+    service = models.ForeignKey(
+        Service, on_delete=models.CASCADE, related_name="chat")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    file = models.FileField(
+        upload_to=group_based_upload_to, blank=True, null=True)
+    added_date = models.DateTimeField(auto_now_add=True)
 
 
 class Payment(models.Model):
-   service = models.ForeignKey(Service, on_delete=models.CASCADE)
-   total_price = models.DecimalField(max_digits=5, decimal_places=2)
-   card_name = models.CharField(max_length=50)
-   card_number = models.CharField(max_length=15)
-   exp_date = models.DateField()
-   paied = models.BooleanField(default=False)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    total_price = models.DecimalField(max_digits=5, decimal_places=2)
+    card_name = models.CharField(max_length=50)
+    card_number = models.CharField(max_length=15)
+    exp_date = models.DateField()
+    paied = models.BooleanField(default=False)
 
 
 class Rating(models.Model):
-   user = models.ForeignKey(User, on_delete=models.CASCADE)
-   service = models.OneToOneField(Service, on_delete=models.CASCADE)
-   comment = models.TextField(max_length=250, blank=True,null=True)
-   rate = models.IntegerField(default=None)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    service = models.OneToOneField(Service, on_delete=models.CASCADE, related_name="rating")
+    comment = models.TextField(max_length=250, blank=True, null=True)
+    rate = models.IntegerField(default=None)
